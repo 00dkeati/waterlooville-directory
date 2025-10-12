ceec68e5c12322c0b47ab57615a255ca208e7a97
00dkeati <deanokeating@gmail.com>
2025-10-12 08:30:17 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
-    return [
-      {
-        source: '/waterlooville-:path*',
-        destination: '/waterlooville-:path*.html',
-      },
-    ]
+    return {
+      beforeFiles: [
+        // These rewrites are checked before pages/public files
+        {
+          source: '/waterlooville-restaurants',
+          destination: '/public/waterlooville-restaurants.html',
+        },
+        {
+          source: '/waterlooville-shops',
+          destination: '/public/waterlooville-shops.html',
+        },
+        {
+          source: '/waterlooville-dentist',
+          destination: '/public/waterlooville-dentist.html',
+        },
+        // Add more specific routes as needed
+      ],
+    }
   },
-  trailingSlash: false,
 }
 
 module.exports = nextConfig
