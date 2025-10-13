126723481a40322b878ddd28302c24bc30b723b4
00dkeati <deanokeating@gmail.com>
2025-10-13 22:28:44 +0100

    Fix editorial images by adding Unsplash to allowed domains

--- next.config.js
+++ next.config.js
@@ -1,5 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'maps.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
   async rewrites() {
     return {
       beforeFiles: [
